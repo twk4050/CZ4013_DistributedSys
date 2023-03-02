cd0da9c2749831d560f400893e5838ace65d30c1
twk97 <twk97@hotmail.com>
2023-03-02 10:38:23 +0800

    add specific fn to marshalling.java

--- src/marshalling.java
+++ src/marshalling.java
@@ -1,29 +1,38 @@
 import entity.Constant;
 
-
 public class marshalling {
-    public static byte[] stringtobyte(String converting, int rnd){
+    public static byte[] stringtobyte(String converting, int rnd) {
         byte[] converted = converting.getBytes(Constant.commonCharset);
         return converted;
-    }    
-    public static String bytetoString(byte[] converting){
+    }
+
+    public static String bytetoString(byte[] converting) {
         String converted = new String(converting, Constant.commonCharset);
         return converted;
     }
 
-    public static String[] decodeMessage(String string){
-        String[] token = string.split("\\|");
-        return token;
-    }
-
-    public static String[] decodedString(String string){
-        String[] tokens = string.split(":");
-        return tokens;
-    }
-
-    public static int msglength(byte[] bytes){
+    public static int msglength(byte[] bytes) {
         String str = bytetoString(bytes);
         return str.length();
+    }
+
+    public static String getRequirementId(String s) {
+        String[] splitColon = s.split(":");
+        String[] caseAndFunctionArgs = splitColon[1].split("\\|");
+
+        return caseAndFunctionArgs[0];
+    }
+
+    public static String getRequestId(String string) {
+        String[] tokens = string.split(":");
+        return tokens[0];
+    }
+
+    public static String[] getCaseAndFnArgs(String string) {
+        String[] tokens = string.split(":");
+        String[] caseAndFnArgs = tokens[1].split("\\|");
+
+        return caseAndFnArgs;
     }
 
 }
