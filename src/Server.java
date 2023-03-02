9adf98c7721adbbe868a90f66b1ee45ec1d100ff
jiaein <jiaeinloo98@gmail.com>
2023-03-03 01:04:44 +0800

    Added invocation semantics and packet loss

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,9 @@
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.List;
+import java.util.Random;
 import java.net.DatagramPacket;
+import java.util.HashMap;
 
 public class Server {
 
@@ -18,6 +20,12 @@ public class Server {
     final String CASE_CANCEL_SEATS = "5";
     final String CASE_GET_FLIGHTS_BELOW_PRICE = "6";
 
+    boolean AT_MOST_ONCE = true;
+    boolean AT_LEAST_ONCE = true;
+
+    final Integer refuseRate = 5;
+    final Integer failSendRate = 5;
+
     // constructor
     // specifiy specific Exception
     public Server(int portNo, FlightOverview fOverview, SClientOverview clientOverview) throws Exception {
@@ -30,148 +38,228 @@ public class Server {
     }
 
     public void startListening() throws Exception {
+        /* At most once invocation semantics 
+         * Initialize to null
+         * If AT_MOST_ONCE is true, initialize HashMap
+        */
+        HashMap<Integer,String> atMostOnceCache = null;
+        if (AT_MOST_ONCE) {
+            atMostOnceCache = new HashMap<Integer,String>();
+            System.out.println("At most once activated.");
+        }
+
         while (true) {
-            this.socket.receive(this.packet); // blocking
+            Integer successRate = getRandomNum();
+            System.out.println("Success Rate for packet receive = " + successRate +"0%");
+            if (successRate >= refuseRate) { //packet is not lost
+                this.socket.receive(this.packet); // blocking
 
-            InetAddress clientAddress = packet.getAddress();
-            int clientPortNo = packet.getPort();
-            SClient client123 = new SClient(clientAddress, clientPortNo);
+                InetAddress clientAddress = packet.getAddress();
+                int clientPortNo = packet.getPort();
+                SClient client123 = new SClient(clientAddress, clientPortNo);
 
-            String msg = Marshalling.convertByteToStringBuilder(this.buffer).toString();
+                String msg = Marshalling.convertByteToStringBuilder(this.buffer).toString();
 
-            /*
-             * some processing here <requestId:requirementId|flightId>
-             * // 59178:1|arg1|arg2|
-             * // 1. process requirementId first to enter case statement
-             * // 2. process again in switch-case to get fn arguments
-             */
+                /*
+                * some processing here <requestId:requirementId|flightId>
+                * // 59178:1|arg1|arg2|
+                * // 1. process requirementId first to enter case statement
+                * // 2. process again in switch-case to get fn arguments
+                */
 
-            String[] caseAndArgs = Marshalling.getCaseAndFnArgs(msg);
-            String caseId = caseAndArgs[0];
+                String[] caseAndArgs = Marshalling.getCaseAndFnArgs(msg);
+                String caseId = caseAndArgs[0];
 
-            System.out.println("caseid: " + caseId + " from: " + clientAddress + "/" + clientPortNo);
+                System.out.println("caseid: " + caseId + " from: " + clientAddress + "/" + clientPortNo);
 
-            switch (caseId) {
-                case CASE_GET_FLIGHT:
-                    String source = caseAndArgs[1];
-                    String dest = caseAndArgs[2];
+                String requestId = Marshalling.getRequestId(msg);
+                boolean duplicated = false;
 
-                    Response123 response1 = this.fOverview.getFlight(source, dest);
+                /*Do AT_MOST_ONCE if it is activated */
+                if (atMostOnceCache != null){
+                    /*Check for duplicated message */
+                    if (atMostOnceCache.containsKey(Integer.parseInt(requestId))){
+                        System.out.println("Duplicated Message " + requestId + "|" + caseAndArgs + " received");
+                        //Include result cache to store all past results and send back client
+                        duplicated = true;
+                        //Send message to client
 
-                    this.sendPacket(client123, response1.getMessage());
-
-                    break;
-                case CASE_GET_FLIGHT_BY_ID:
-                    int flightId = Integer.parseInt(caseAndArgs[1]);
-
-                    Response123 response2 = this.fOverview.getFlightById(flightId);
-                    this.sendPacket(client123, response2.getMessage());
-
-                    break;
-                case CASE_RESERVE_SEAT:
-                    int flightIdToReserve = Integer.parseInt(caseAndArgs[1]);
-                    int numberOfSeats = Integer.parseInt(caseAndArgs[2]);
-                    String username = caseAndArgs[3];
-                    String password = caseAndArgs[4];
-
-                    client123.setUsername(username); // ???
-                    client123.setPassword(password); // ???
-                    int userId = clientOverview.checkUserExistAndValidate(username, password);
-                    if (userId != -1) {
-                        Response123 response3 = this.fOverview.reserveSeat(flightIdToReserve, numberOfSeats, client123);
-
-                        // either flightId wrong or unable to reserve seats
-                        if (response3.getStatus() == "-1") {
-                            this.sendPacket(client123, response3.getMessage());
-                            this.refreshPacket();
-                            continue;
-                        }
-
-                        // if successful
-                        String returnMsg = response3.getMessage();
-
-                        this.sendPacket(client123, returnMsg);
-                        this.refreshPacket();
-
-                        List<SClient> monitorList = response3.getMonitorList();
-                        for (SClient c : monitorList) {
-                            long currentTimestamp = System.currentTimeMillis();
-
-                            if (c.getMonitorEndTime() < currentTimestamp) {
-                                this.sendPacket(c, "Client removed from monitorList");
-                                this.refreshPacket();
-                            } else {
-                                this.sendPacket(c, returnMsg);
-                                this.refreshPacket();
+                        /*At least once invocation semantics */
+                        if (AT_LEAST_ONCE){
+                            Integer atLeastOnceRate = getRandomNum();
+                            System.out.println("At Least Once Rate = " + atLeastOnceRate +"0%");
+                            if (atLeastOnceRate >= failSendRate) { //Message able to send
+                                System.out.println("Success message");
+                            }  else{ //Message unable to send
+                                System.out.println("Fail message");
                             }
+                        } else{
+                            System.out.println("At least once not activated.");
                         }
-                        response3.getFlight().removeExpiredFromMonitorList();
 
-                    } else {
-                        // send error message
-                        this.sendPacket(client123, "user does not exist");
                     }
-                    break;
-                case CASE_MONITOR_FLIGHT:
-                    int flightId4 = Integer.parseInt(caseAndArgs[1]);
-                    long duration = Long.parseLong(caseAndArgs[2]); // getting input in seconds
+                    /*Not duplicated message */
+                    else {
+                        atMostOnceCache.put(Integer.parseInt(requestId), caseAndArgs[1]);
+                        duplicated = false;
+                    }
+                } 
+                /*Skip AT_MOST_ONCE if not activated */
 
-                    client123.setMonitorEndTimeInMs(duration);
-
-                    Response123 response4 = this.fOverview.monitorFlight(flightId4, client123);
-                    this.sendPacket(client123, response4.getMessage());
-
-                    break;
-                case CASE_CANCEL_SEATS:
-                    int flightIdToCancel = Integer.parseInt(caseAndArgs[1]);
-                    String username5 = caseAndArgs[2];
-                    String password5 = caseAndArgs[3];
-
-                    client123.setUsername(username5); // ???
-                    client123.setPassword(password5); // ???
-                    int userId5 = clientOverview.checkUserExistAndValidate(username5, password5);
-                    if (userId5 != -1) {
-
-                        Response123 response5 = this.fOverview.cancelSeat(flightIdToCancel, client123);
-
-                        String returnMsg = response5.getMessage();
-
-                        this.sendPacket(client123, returnMsg);
-                        this.refreshPacket();
-
-                        List<SClient> monitorList = response5.getMonitorList();
-                        for (SClient c : monitorList) {
-                            long currentTimestamp = System.currentTimeMillis();
-
-                            if (c.getMonitorEndTime() < currentTimestamp) {
-                                this.sendPacket(c, "Client removed from monitorList");
-                                this.refreshPacket();
-                            } else {
-                                this.sendPacket(c, returnMsg);
-                                this.refreshPacket();
+                if (duplicated == false){
+                    switch (caseId) {
+                        case CASE_GET_FLIGHT:
+                            String source = caseAndArgs[1];
+                            String dest = caseAndArgs[2];
+        
+                            Response123 response1 = this.fOverview.getFlight(source, dest);
+        
+                            /*At least once invocation semantics */
+                            if (AT_LEAST_ONCE){
+                                Integer atLeastOnceRate = getRandomNum();
+                                System.out.println("At Least Once Rate = " + atLeastOnceRate +"0%");
+                                if (atLeastOnceRate >= failSendRate) { //Message able to send
+                                    System.out.println("Success message");
+                                    this.sendPacket(client123, response1.getMessage());
+                                }  else{ //Message unable to send
+                                    System.out.println("Message failed to send");
+                                }
+                            } else{
+                                System.out.println("At least once not activated.");
+                                this.sendPacket(client123, response1.getMessage());
                             }
-                        }
-                        response5.getFlight().removeExpiredFromMonitorList();
-                    } else {
-                        // send error message
-                        this.sendPacket(client123, "user does not exist");
-                    }
+        
+                            break;
+                        case CASE_GET_FLIGHT_BY_ID:
+                            int flightId = Integer.parseInt(caseAndArgs[1]);
+        
+                            Response123 response2 = this.fOverview.getFlightById(flightId);
 
-                    break;
-                case CASE_GET_FLIGHTS_BELOW_PRICE:
-                    Float priceThreshold = Float.parseFloat(caseAndArgs[1]);
-
-                    Response123 response6 = this.fOverview.getFlightBelowCertainPrice(priceThreshold);
-
-                    this.sendPacket(client123, response6.getMessage());
-
-                    break;
-                default:
-                    System.out.println("in switch-case default statement: " + caseId);
-                    this.sendPacket(client123, "echo Server: " + caseId);
-                    break;
+                            /*At least once invocation semantics */
+                            if (AT_LEAST_ONCE){
+                                Integer atLeastOnceRate = getRandomNum();
+                                System.out.println("At Least Once Rate = " + atLeastOnceRate +"0%");
+                                if (atLeastOnceRate >= failSendRate) { //Message able to send
+                                    System.out.println("Success message");
+                                    this.sendPacket(client123, response2.getMessage());
+                                }  else{ //Message unable to send
+                                    System.out.println("Message failed to send");
+                                }
+                            } else{
+                                System.out.println("At least once not activated.");
+                                this.sendPacket(client123, response2.getMessage());
+                            }                       
+        
+                            break;
+                        case CASE_RESERVE_SEAT:
+                            int flightIdToReserve = Integer.parseInt(caseAndArgs[1]);
+                            int numberOfSeats = Integer.parseInt(caseAndArgs[2]);
+                            String username = caseAndArgs[3];
+                            String password = caseAndArgs[4];
+        
+                            client123.setUsername(username); // ???
+                            client123.setPassword(password); // ???
+                            int userId = clientOverview.checkUserExistAndValidate(username, password);
+                            if (userId != -1) {
+                                Response123 response3 = this.fOverview.reserveSeat(flightIdToReserve, numberOfSeats, client123);
+        
+                                // either flightId wrong or unable to reserve seats
+                                if (response3.getStatus() == "-1") {
+                                    this.sendPacket(client123, response3.getMessage());
+                                    this.refreshPacket();
+                                    continue;
+                                }
+        
+                                // if successful
+                                String returnMsg = response3.getMessage();
+        
+                                this.sendPacket(client123, returnMsg);
+                                this.refreshPacket();
+        
+                                List<SClient> monitorList = response3.getMonitorList();
+                                for (SClient c : monitorList) {
+                                    long currentTimestamp = System.currentTimeMillis();
+        
+                                    if (c.getMonitorEndTime() < currentTimestamp) {
+                                        this.sendPacket(c, "Client removed from monitorList");
+                                        this.refreshPacket();
+                                    } else {
+                                        this.sendPacket(c, returnMsg);
+                                        this.refreshPacket();
+                                    }
+                                }
+                                response3.getFlight().removeExpiredFromMonitorList();
+        
+                            } else {
+                                // send error message
+                                this.sendPacket(client123, "user does not exist");
+                            }
+                            break;
+                        case CASE_MONITOR_FLIGHT:
+                            int flightId4 = Integer.parseInt(caseAndArgs[1]);
+                            long duration = Long.parseLong(caseAndArgs[2]); // getting input in seconds
+        
+                            client123.setMonitorEndTimeInMs(duration);
+        
+                            Response123 response4 = this.fOverview.monitorFlight(flightId4, client123);
+                            this.sendPacket(client123, response4.getMessage());
+        
+                            break;
+                        case CASE_CANCEL_SEATS:
+                            int flightIdToCancel = Integer.parseInt(caseAndArgs[1]);
+                            String username5 = caseAndArgs[2];
+                            String password5 = caseAndArgs[3];
+        
+                            client123.setUsername(username5); // ???
+                            client123.setPassword(password5); // ???
+                            int userId5 = clientOverview.checkUserExistAndValidate(username5, password5);
+                            if (userId5 != -1) {
+        
+                                Response123 response5 = this.fOverview.cancelSeat(flightIdToCancel, client123);
+        
+                                String returnMsg = response5.getMessage();
+        
+                                this.sendPacket(client123, returnMsg);
+                                this.refreshPacket();
+        
+                                List<SClient> monitorList = response5.getMonitorList();
+                                for (SClient c : monitorList) {
+                                    long currentTimestamp = System.currentTimeMillis();
+        
+                                    if (c.getMonitorEndTime() < currentTimestamp) {
+                                        this.sendPacket(c, "Client removed from monitorList");
+                                        this.refreshPacket();
+                                    } else {
+                                        this.sendPacket(c, returnMsg);
+                                        this.refreshPacket();
+                                    }
+                                }
+                                response5.getFlight().removeExpiredFromMonitorList();
+                            } else {
+                                // send error message
+                                this.sendPacket(client123, "user does not exist");
+                            }
+        
+                            break;
+                        case CASE_GET_FLIGHTS_BELOW_PRICE:
+                            Float priceThreshold = Float.parseFloat(caseAndArgs[1]);
+        
+                            Response123 response6 = this.fOverview.getFlightBelowCertainPrice(priceThreshold);
+        
+                            this.sendPacket(client123, response6.getMessage());
+        
+                            break;
+                        default:
+                            System.out.println("in switch-case default statement: " + caseId);
+                            this.sendPacket(client123, "echo Server: " + caseId);
+                            break;
+                }
+                this.refreshPacket();
+                }
+            }  else{ //packet is lost
+                System.out.println("Simulating packet loss");
             }
-            this.refreshPacket();
+            
         }
     }
 
@@ -188,5 +276,10 @@ public class Server {
     public void refreshPacket() {
         this.buffer = new byte[65535];
         this.packet = new DatagramPacket(this.buffer, this.buffer.length);
+    }
+
+    public static int getRandomNum(){
+        Random rand = new Random();
+        return rand.nextInt(10);
     }
 }
