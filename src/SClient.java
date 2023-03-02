1e31fc2d757f848a151564ac4951201df3e7a625
twk97 <twk97@hotmail.com>
2023-03-02 11:44:41 +0800

    update server client file to cater for username/pw

--- src/SClient.java
+++ src/SClient.java
@@ -20,23 +20,63 @@ public class SClient {
         this.portNo = portNo;
 
         long currentTimestamp = System.currentTimeMillis();
-        this.monitorEndTime = currentTimestamp + durationToMonitor * 1000; // TODO: client inputs 100second, multiply
+        this.monitorEndTime = currentTimestamp + durationToMonitor * 1000; // TODO: client inputs 100second, multiply //
                                                                            // 1000 to make it milliseconds
+    }
+
+    public SClient(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
     public int getId() {
         return this.Id;
     }
 
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public InetAddress getAddress() {
         return this.address;
+    }
+
+    public void setAddress(InetAddress address) {
+        this.address = address;
     }
 
     public int getPortNo() {
         return this.portNo;
     }
 
+    public void setPortNo(int portNo) {
+        this.portNo = portNo;
+    }
+
     public long getMonitorEndTime() {
         return this.monitorEndTime;
+    }
+
+    public void setMonitorEndTime(long monitorEndTime) {
+        this.monitorEndTime = monitorEndTime;
+    }
+
+    public boolean validateUser(String usernameInput, String passwordInput) {
+        if (this.username.equals(usernameInput) && this.password.equals(passwordInput)) {
+            return true;
+        }
+        return false;
     }
 }
